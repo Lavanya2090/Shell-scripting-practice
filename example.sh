9434a2245522708d2c94a174b8c192a4c77f221b
Lavanya2090 <chlavanyavenkata@gmail.com>
2025-07-05 23:10:27 +0530

    initial commit

---
+++ example.sh
@@ -0,0 +1,10 @@
+echo "Hello Lav welcome to git world"
+
+# Create a new directory named temp
+mkdir -p temp
+# Create 5 files inside the temp directory
+for i in {1..5}
+do
+  touch temp/file$i.txt
+done
+
