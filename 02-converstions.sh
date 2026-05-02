c58367ffd25397e38f1b6eeb409d2341b1c4c20b
Lavanya2090 <chlavanyavenkata@gmail.com>
2026-05-02 18:13:27 +0530

    coneversations done

---
+++ 02-converstions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Lav:how are you, what are you doing"
+echo "Naren: i am good, i am doing my work"
+echo "Lav: what work are you doing"
+echo "Naren: i am doing my shell scripting practice"
