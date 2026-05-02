ee40b3983b71fdebc2cd90481e28bed89dc51161
Lavanya2090 <chlavanyavenkata@gmail.com>
2026-05-02 18:29:01 +0530

    variables done

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=LAV #NO SPACE BEWEEN VARIABLE NAME AND VALUE     
+PERSON2=NAREN
+    
+echo "PERSON1:how are you, what are you doing"
+echo "PERSON2: i am good, i am doing my work"
+echo "PERSON1: what work are you doing"
+echo "PERSON2: i am doing my shell scripting practice"
